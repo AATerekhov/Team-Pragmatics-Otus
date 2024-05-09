1b15ef3488c4471ddf432a00ba96119a0d7f6f07
KennyDew <dubrovskiynv@gmail.com>
2024-05-09 18:27:34 +0300

    Solution Recovery

---
+++ 04_TravelCard/Domain.Entities/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    internal interface IUser
+    {
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        [Key, Column("user_ID")]
+        public int Id { get; set; }
+
+        [Column("travel_ID")]
+        public int TravelId { get; set; }
+
+        public Travel Travel { get; set; }
+    }
+}
---
+++ 04_TravelCard/Infrastructure.EntityFramework/DataContext.cs
@@ -0,0 +1,52 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infrastructure.EntityFramework
+{
+    internal class DataContext : DbContext
+    {
+        public DbSet<Manager> Managers { get; set; }
+
+        public DbSet<Travel> Travels { get; set; }
+
+        public DbSet<TravelPoint> TravelPoints { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DataContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Связь 1 к 1 (Путешествие - менеджер)
+            modelBuilder.Entity<Travel>()
+                .HasOne(t => t.Manager)
+                .WithOne(m => m.Travel)
+                .HasForeignKey<Manager>(m => m.TravelId)
+                .IsRequired();
+
+            // Связь 1 к n (Путешествие - компаньоны)
+            modelBuilder.Entity<Travel>()
+                .HasMany(t => t.Users)
+                .WithOne(u => u.Travel)
+                .HasForeignKey(u => u.TravelId);
+
+            // Связь 1 к n (Путешествие - остановки)
+            modelBuilder.Entity<Travel>()
+                .HasMany(t => t.TravelPoints)
+                .WithOne(tp => tp.Travel)
+                .HasForeignKey (tp => tp.TravelId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Username=postgres;Password=admin3725;Database=TravelCard_db");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
