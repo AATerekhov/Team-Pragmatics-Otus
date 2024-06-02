8e1057cfd3fb80826997a290d95b3e6d6e9c6045
AATerekhov <A.A.Terekhov@mail.ru>
2024-06-03 00:02:32 +0300

    Add worling Edit PleceType

--- WebInterface/api-work.js
+++ WebInterface/api-work.js
@@ -1,5 +1,5 @@
 async function loadPlaceType(){
-    let result =await fetch("http://localhost:49759/api/PlaceType");
+    let result =await fetch("http://localhost:52199/api/PlaceType");
     let table = document.getElementById('placeTypes');
     if(result.ok){
         let placetypes = await result.json();
@@ -11,10 +11,15 @@ async function loadPlaceType(){
            nameField.innerText = element.name;
            let descriptionField = document.createElement('td');
            descriptionField.innerText = element.description;
-
+           let link = document.createElement('a');
+           link.innerText = "Редактировать";
+           link.href = `editPlaceType.html?id=${element.id}`;
+           let editField = document.createElement('td');
+           editField.appendChild(link);
            placetypeString.appendChild(idField);
            placetypeString.appendChild(nameField);
            placetypeString.appendChild(descriptionField);
+           placetypeString.appendChild(editField);
            table.appendChild(placetypeString);
         });
     }
@@ -22,7 +27,7 @@ async function loadPlaceType(){
 }
 async function loadPlace(idPlaceType)
 {
-    let result =await fetch(`http://localhost:49759/api/Place/${idPlaceType}`);
+    let result =await fetch(`http://localhost:52199/api/Place/${idPlaceType}`);
     let table = document.getElementById('places');
     if(result.ok){
         let places = await result.json();
@@ -38,7 +43,6 @@ async function loadPlace(idPlaceType)
            longitudeField.innerText = element.longitude;
            let latitudeField = document.createElement('td');
            latitudeField.innerText = element.latitude;
-
            placetypeString.appendChild(idField);
            placetypeString.appendChild(nameField);
            placetypeString.appendChild(descriptionField);
@@ -49,16 +53,83 @@ async function loadPlace(idPlaceType)
     }
     console.log(result);
 }
-
 async function createPlaceType(){
     let name = document.getElementById('name').value;
     let description = document.getElementById('description').value;
     let placeType = {name, description};
-    let result = await fetch('http://localhost:49759/api/PlaceType',{
+    let result = await fetch('http://localhost:52199/api/PlaceType',{
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(placeType)
     });
     if(result.ok)
         window.location.href = "index.html";
+}
+
+async function getPlaceTypeByIdAsync(id){
+    let rezult = await fetch(`http://localhost:52199/api/PlaceType/${id}`)
+    if(rezult.ok)
+        return await rezult.json();
+}
+async function updatePlaceTypeLoad(){
+    let id = new URLSearchParams(window.location.search).get('id');
+    let selectPlaceType = await getPlaceTypeByIdAsync(id);
+    let nameField = document.getElementById('name');
+    let descriptionField = document.getElementById('description');
+    nameField.value = selectPlaceType.name;
+    descriptionField.value = selectPlaceType.description;
+}
+async function updatePlaceType(){
+    let id = new URLSearchParams(window.location.search).get('id');
+    let description = document.getElementById('description').value;
+    let placeType = {description};
+    let result = await fetch(`http://localhost:52199/api/PlaceType/${id}`,{
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(placeType)
+    });
+    if(result.ok)
+        window.location.href = "index.html";
+}
+
+async function initMap() {
+    await ymaps3.ready;
+
+    const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker} = ymaps3;
+
+    const map = new YMap(
+        document.getElementById('map'),
+        {
+            location: {
+                center: [30.314997, 59.938784 ],
+                zoom: 10
+            },
+        behaviors: ['drag', 'scrollZoom', 'pinchZoom', 'dblClick']
+        }
+    );
+
+    // Добавьте слой с дорогами и зданиями
+    map.addChild(new YMapDefaultSchemeLayer());
+
+    // Добавьте слой для маркеров
+    map.addChild(new YMapDefaultFeaturesLayer());
+
+    // Создайте DOM-элемент для содержимого маркера.
+    // Важно это сделать до инициализации маркера!
+    // Элемент можно создавать пустым. Добавить HTML-разметку внутрь можно после инициализации маркера.
+    const content = document.createElement('section');
+
+    // Инициализируйте маркер
+    const marker = new YMapMarker(
+      {
+        coordinates: [30.314997, 59.938784 ],
+        draggable: true
+      },
+      content
+    );
+    // Добавьте маркер на карту
+    map.addChild(marker);
+  
+    // Добавьте произвольную HTML-разметку внутрь содержимого маркера
+    content.innerHTML = '<h3>Новое место.</h3>';
 }
