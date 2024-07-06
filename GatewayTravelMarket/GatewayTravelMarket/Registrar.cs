774234a57cf298d7c7f707643de5db042b40be3a
AATerekhov <A.A.Terekhov@mail.ru>
2024-07-06 22:56:15 +0300

    Added general settings and the first controller Созданы общие настройки и добавлен первый метод кондтроллера

---
+++ GatewayTravelMarket/GatewayTravelMarket/Registrar.cs
@@ -0,0 +1,26 @@
+﻿using GatewayTravelMarket.Settings;
+
+namespace GatewayTravelMarket
+{
+    public static  class Registrar
+    {
+        public static IServiceCollection AddHttpClients(this IServiceCollection services, IConfiguration configuration)
+        {
+            //TODO: Переделать получениея Settings.
+            services.AddHttpClient("ProductServiceClient", c => c.BaseAddress = new Uri("http://localhost:52199"));
+            services.AddHttpClient("OrderServiceClient", c => c.BaseAddress = new Uri("http://localhost:52200"));            
+            services.Configure<ApiGatewaySettings>(options =>
+            {
+                var configuration = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+                options.TravelCardServiceBaseUrl = configuration["ApiGatewaySettings:TravelCardServiceBaseUrl"];
+                options.GeoMapServiceBaseUrl = configuration["ApiGatewaySettings:GeoMapServiceBaseUrl"];
+                options.ValidApiKeys = configuration["ApiGatewaySettings:ValidApiKeys"];
+
+            });
+            return services;
+        }
+    }
+}
