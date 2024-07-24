87dbaf2854d9c912a071f12260966a5beeba1c21
AATerekhov <A.A.Terekhov@mail.ru>
2024-07-25 01:31:25 +0300

    linear algebra methods have been added Добавлены структуры линейной алгебры.

---
+++ 02_GeoMap/Infrastructure.Repositories.Implementations/GeometricModel/Point.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Implementations.GeometricModel
+{
+    public struct Point
+    {
+        public double X;
+        public double Y;
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public static Point GetPoint(RoadPoint roadPoint) { return new Point(roadPoint.Longitude, roadPoint.Latitude); }
+
+        public static Point operator +(Point a, Vector b) { return new Point(a.X + b.X, a.Y + b.Y); }
+        public static Point operator -(Point a, Vector b) { return new Point(a.X - b.X, a.Y - b.Y); }
+    }
+}
---
+++ 02_GeoMap/Infrastructure.Repositories.Implementations/GeometricModel/Vector.cs
@@ -0,0 +1,58 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Implementations.GeometricModel
+{
+    public struct Vector
+    {
+        public double X;
+        public double Y;
+
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void Normalize() 
+        {
+            var length = this.GetLength();
+            X = X / length;
+            Y = Y / length;
+        }
+        public void Normalize(double value)
+        {
+            var length = this.GetLength();
+            X = value * X / length;
+            Y = value * Y / length;
+        }
+        public void Rotation(double angle)
+        {
+            X = X * Math.Cos(angle) - Y * Math.Sin(angle);
+            Y = X * Math.Sin(angle) + Y * Math.Cos(angle);
+        }
+
+        public double GetLength() 
+        {
+            return Math.Sqrt(X * X + Y * Y);            
+        }
+
+        public static Vector GetVector(RoadPoint roadPoint) { return new Vector(roadPoint.Longitude, roadPoint.Latitude); }       
+        public static Vector operator +(Vector a, Vector b) 
+        {
+            return new Vector(a.X+b.X, a.Y+b.Y);
+        }
+        public static Vector operator -(Vector a, Vector b)
+        {
+            return new Vector(a.X - b.X, a.Y - b.Y);
+        }
+        public static Vector operator *(Vector a, double b)
+        {
+            return new Vector(a.X*b, a.Y*b);
+        }
+    }
+}
