60f8b95692e938aed41b141d5fcc66d322afa9ea
AATerekhov <A.A.Terekhov@mail.ru>
2024-08-10 11:59:27 +0300

    Add Create operations TravelPoint  WebInterface. Добавлен интерфейс создания точек путешествия в редакторе карточки путешествия.

--- WebInterface/Travel/api-work.js
+++ WebInterface/Travel/api-work.js
@@ -59,7 +59,6 @@ async function travelCreating(){
     
  }
 
-
  //Yandex Map function
 async function initMap() {    
     await ymaps3.ready;
@@ -159,7 +158,7 @@ async function initUpdateMap() {
     let travel = await getTravelByIdAsync(travelId);
     // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
     await ymaps3.ready;
-    const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer,YMapFeature} = ymaps3;
+    const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer,YMapFeature,YMapControls,YMapControlButton,YMapListener} = ymaps3;    
     
     let TrDescField = document.getElementById('description');
     TrDescField.value = travel.description;
@@ -176,10 +175,14 @@ async function initUpdateMap() {
         latitude: startLat};
 
         function onDragMoveHandlerSP(coordinates) { 
-            //strpnt.value = coordinates;
-            lngSP.value = coordinates[0].toFixed(6);            
+            lngSP.value = coordinates[0].toFixed(6); 
+            startLng = coordinates[0].toFixed(6);   
             latSP.value = coordinates[1].toFixed(6);
-            draggableStartPoint.update({coordinates});
+            startLat = coordinates[1].toFixed(6);
+            line.update({ geometry: {
+                type: 'LineString',
+                coordinates: [coordinates,[finishLng, finishLat]]
+              }});
         }
     
     let lngFP = document.getElementById('longitudeFP');
@@ -195,9 +198,14 @@ async function initUpdateMap() {
 
         function onDragMoveHandlerEP(coordinates) { 
             //strpnt.value = coordinates;
-            lngFP.value = coordinates[0].toFixed(6);            
+            lngFP.value = coordinates[0].toFixed(6);    
+            finishLng = coordinates[0].toFixed(6);  
             latFP.value = coordinates[1].toFixed(6);
-            draggableFinishPoint.update({coordinates});
+            finishLat = coordinates[1].toFixed(6);
+            line.update({ geometry: {
+                type: 'LineString',
+                coordinates: [[startLng, startLat],coordinates]
+              }});
         }    
     
     // Иницилиазируем карту
@@ -208,7 +216,6 @@ async function initUpdateMap() {
         {
             location: {
                 // Координаты центра карты
-                //center: [37.588144, 55.733842],
                 center: [startLng/2 + finishLng/2, startLat/2 + finishLat/2],
                 // Уровень масштабирования
                 zoom: Number(scale)
@@ -223,88 +230,96 @@ async function initUpdateMap() {
     // Добавьте слой для маркеров
     map.addChild(new YMapDefaultFeaturesLayer({zIndex: 1800}));
 
+    const clickCallback = (object,event) => {
+        let lngP = document.getElementById('longitudeP');
+        lngP.value = event.coordinates[0].toFixed(6);
+        let latP = document.getElementById('latitudeP');
+        latP.value = event.coordinates[1].toFixed(6);
+    }
+    const listener = new YMapListener({
+        layer: 'any',
+        onClick: clickCallback
+    });
+    map.addChild(listener);
+    
+    
     // Import the package to add a default marker
     const {YMapDefaultMarker} = await ymaps3.import('@yandex/ymaps3-markers@0.0.1');
     // Create default markers and add them to the map
-    const draggableStartPoint  = new YMapDefaultMarker({
-        coordinates: [startLng, startLat],
-        title: `Начало путешествия`,
-        subtitle: 'Перетащите метку',
-        color: '#0047FF',
-        draggable: true,
-        onDragMove: onDragMoveHandlerSP
-      });
-      map.addChild(draggableStartPoint); 
-  
-    const draggableFinishPoint  = new YMapDefaultMarker({
-        coordinates: [finishLng, finishLat],
-        title: `Конечная точка`,
-        subtitle: 'Перетащите метку',
-        color: '#0047FF',
-        draggable: true,
-        onDragMove: onDragMoveHandlerEP
-      });
-      map.addChild(draggableFinishPoint);      
-      
-     const line = new YMapFeature({
-        geometry: {
-          type: 'LineString',
-          coordinates: [[startLng, startLat],
-                        [finishLng, finishLat]]
-        },
-        style: {stroke: [{color: '#0047FF', width: 4}]}
-      });
-      map.addChild(line);
+    const draggableStartPoint  = CreateDragMarker( [startLng, startLat],`Начало путешествия`,onDragMoveHandlerSP);  
+    const draggableFinishPoint = CreateDragMarker( [finishLng, finishLat],`Конечная точка`,onDragMoveHandlerEP);
+    const line = CreateLine([[startLng, startLat],[finishLng, finishLat]]);
+    map.addChild(draggableStartPoint); 
+    map.addChild(draggableFinishPoint);    
+    map.addChild(line);  
+    
+    let places = await GetTrasingPlaces(start,finish,placeTypeId);
+    places.forEach(element => {   
+      let insertPoint = [element.longitude,element.latitude];
+      const draggableMarker  = new YMapDefaultMarker({
+       coordinates: insertPoint,
+       color: '#00CC00',
+       popup: {content: `${element.name}<br> 
+           ${element.description.split(" |",8)[0]}<br> 
+           ${element.description.split(" |",8)[1]}<br> 
+           ${element.description.split(" |",8)[2]}<br> 
+           ${element.description.split(" |",8)[3]}<br> 
+           ${element.description.split(" |",8)[4]}<br> 
+           ${element.description.split(" |",8)[5]}<br> 
+           ${element.description.split(" |",8)[6]}<br> 
+           ${element.description.split(" |",8)[7]}<br> 
+           ${element.longitude}, ${element.latitude}<br>             
+           `, position: 'left'}
+        });
+        map.addChild(draggableMarker ); 
+    });
 
-      let places = await GetTrasingPlaces(start,finish,placeTypeId);
-      places.forEach(element => {   
-        let insertPoint = [element.longitude,element.latitude];
-        const draggableMarker  = new YMapDefaultMarker({
-         coordinates: insertPoint,
-         //title: `${element.name}`,
-         //subtitle: 'Place',
-         color: '#00CC00',
-         popup: {content: `${element.name}<br> 
-             ${element.description.split(" |",8)[0]}<br> 
-             ${element.description.split(" |",8)[1]}<br> 
-             ${element.description.split(" |",8)[2]}<br> 
-             ${element.description.split(" |",8)[3]}<br> 
-             ${element.description.split(" |",8)[4]}<br> 
-             ${element.description.split(" |",8)[5]}<br> 
-             ${element.description.split(" |",8)[6]}<br> 
-             ${element.description.split(" |",8)[7]}<br> 
-             ${element.longitude}, ${element.latitude}<br>             
-             `, position: 'left'}
-       });
-       map.addChild(draggableMarker ); 
-     });
+    const bottomControls = new YMapControls({position: 'bottom'});
+    map.addChild(bottomControls);
+    let typePlaces =await GetPlaceTypes();
+    const buttoms = [];
+    typePlaces.forEach(element => {
+        const button = new YMapControlButton({
+            text: element.name,
+            color: '#fff',
+            background: '#007afce6',
+            onClick: () => window.location.href = `updateTravel.html?typeId=${element.id}&Scale=8&id=${travelId}`
+          });
+          buttoms.push(button);
+    });
+    buttoms.forEach(element => 
+        bottomControls.addChild(element));    
+
+    function CreateLine(points)
+     {
+        return new YMapFeature({
+            geometry: {
+              type: 'LineString',
+              coordinates: points
+            },
+            style: {stroke: [{color: '#0047FF', width: 4}]}
+          });
+     }
+     function CreateDragMarker( point, title ,onDragMoveHandler)
+     {
+       return new YMapDefaultMarker({
+           coordinates: point,
+           title: title,
+           subtitle: 'Перетащите метку',
+           color: '#0047FF',
+           draggable: true,
+           onDragMove: onDragMoveHandler
+         });
+     }  
 }
 
-async function AddButton() {    
-    let id = new URLSearchParams(window.location.search).get('id');
-     //Заполняем таблицу placeTypes кнопками.
-     let resultType =await fetch("http://localhost:5200/api/placetypes",{
+async function GetPlaceTypes() {
+    let resultType =await fetch("http://localhost:5200/api/placetypes",{
         method: 'GET',
         headers: {'TM-API-Key': 'F504ED6B-68AA-456C-B839-C1559ACED2EF'}
     });
-    if(resultType.ok){
-        let placetypes = await resultType.json();
-        placetypes.forEach(element => {           
-           createButton(document.body,element.name,element.id,id);           
-        });
-    } 
-}
-
-function createButton(context, name , typeId, id) {
-    var button = document.createElement("input");
-    button.type = "button";
-    button.value = name;
-    button.onclick = function openTravelUpdate() {     
-        window.location.href = `updateTravel.html?typeId=${typeId}&Scale=8&id=${id}`;  
-        return false;      
-     }
-    button.disabled = false;
-    context.appendChild(button);
+    if(resultType.ok)
+        return await resultType.json();
 }
 //id - тип мест трассировки
 async function GetTrasingPlaces(start,finish,id) {    
@@ -319,4 +334,22 @@ async function GetTrasingPlaces(start,finish,id) {
     });
     if(result.ok)
         return await result.json();
+}
+async function  addPoint() {
+    let travelId = new URLSearchParams(window.location.search).get('id');
+    const lngP = Number(document.getElementById('longitudeP').value);
+    const latP = Number(document.getElementById('latitudeP').value);
+    let waitingTimeCountMinutes = Number(document.getElementById('timeP').value);
+    let pointDesc = document.getElementById('descP').value;
+    let pointMap = `${lngP},${latP}`;
+    let trevelPoint = {pointMap,pointDesc,travelId,waitingTimeCountMinutes};
+    let result = await fetch(`http://localhost:5200/api/point`,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'TM-API-Key': 'F504ED6B-68AA-456C-B839-C1559ACED2EF'},
+        body: JSON.stringify(trevelPoint)
+    });
+    if(result.ok)
+        window.location.href = `updateTravel.html?typeId=0&Scale=8&id=${travelId}`
 }
