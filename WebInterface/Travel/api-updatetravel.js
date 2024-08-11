8473a0df563cfaf9bc533a994410a32a54510704
AATerekhov <A.A.Terekhov@mail.ru>
2024-08-11 23:57:26 +0300

    Edit interface befor Retro Доработка интрефейса для ретро 12.08.

--- WebInterface/Travel/api-updatetravel.js
+++ WebInterface/Travel/api-updatetravel.js
@@ -91,8 +91,29 @@ async function initUpdateMap() {
         let lngP = document.getElementById('longitudeP');
         lngP.value = event.coordinates[0].toFixed(6);
         let latP = document.getElementById('latitudeP');
-        latP.value = event.coordinates[1].toFixed(6);
+        latP.value = event.coordinates[1].toFixed(6); 
+        let desc = document.getElementById('descP');
+        let time = document.getElementById('timeP');
+        const decsPoint = sorted(event.coordinates);
+        if (decsPoint != ""){
+            desc.value = decsPoint;
+            time.value = 10;
+        } else {
+            desc.value = "";
+            time.value = "";
+        }
     }
+    function sorted(coordinates)
+    {
+        let result = "";
+        places.forEach(element => {
+            const distance =Math.sqrt((element.longitude - coordinates[0])**2 + (element.latitude - coordinates[1])**2);
+            if (distance < 0.001) 
+                result = element.description;
+        });
+        return result;
+    }
+
     const listener = new YMapListener({
         layer: 'any',
         onClick: clickCallback
@@ -105,12 +126,43 @@ async function initUpdateMap() {
     {
         let maps = [[startLng, startLat]];
         points.forEach(element => {
+            if(element.waitingTimeCountMinutes == 0)
             maps.push([Number(element.pointMap.split(',')[0]),Number(element.pointMap.split(',')[1])]);
         });
         maps.push([finishLng, finishLat]);
         return maps;
     }
 
+    class HintWindow extends ymaps3.YMapComplexEntity {
+        _element;
+        _detachDom;
+        _unwatchSearchContext;
+       // Method for create a DOM control element
+       _createElement() {
+           const windowElement = document.createElement('div');
+           windowElement.className = 'hintWindow';
+           return windowElement;
+       }
+
+       // Callback method triggered on hint context change, responsible for updating the text in the hint window
+       _searchContextListener() {
+           this._element.innerHTML = this._consumeContext(YMapHintContext)?.hint;
+       }
+
+       // Method for attaching the control to the map
+        _onAttach() {
+           this._element = this._createElement();
+           this._unwatchSearchContext = this._watchContext(YMapHintContext, this._searchContextListener.bind(this));
+           this._detachDom = ymaps3.useDomContext(this, this._element, this._element);
+       }
+
+       // Method for detaching control from the map
+        _onDetach() {
+           this._unwatchSearchContext();
+           this._detachDom();
+       }
+   }
+    const {YMapHint, YMapHintContext} = await ymaps3.import('@yandex/ymaps3-hint@0.0.1');
     // Import the package to add a default marker
     const {YMapDefaultMarker} = await ymaps3.import('@yandex/ymaps3-markers@0.0.1');
     // Create default markers and add them to the map
@@ -119,26 +171,46 @@ async function initUpdateMap() {
     const line = CreateLine(mapPoints);
     map.addChild(draggableStartPoint); 
     map.addChild(draggableFinishPoint);    
-    map.addChild(line);  
-    
-    if(placeTypeId !== 0){
-        let places = await GetTrasingPlaces(ConvertMapPointsInTracing(mapPoints),placeTypeId);
+    map.addChild(line);
+    let places=[];
+    if(placeTypeId != 0){
+        places = await GetTrasingPlaces(ConvertMapPointsInTracing(mapPoints),placeTypeId);
         places.forEach(element => {   
           let insertPoint = [element.longitude,element.latitude];
           const draggableMarker  = new YMapDefaultMarker({
            coordinates: insertPoint,
+           properties: {
+            hint: `${getComment(element.description)}`
+          },
            color: '#00CC00',
-           popup: {content: `${getComment(element.description)}           
-               `, position: 'left'}
-            });
+           //popup: {content: `${getComment(element.description)}`, position: 'left'}
+        });
             map.addChild(draggableMarker ); 
         });
-    }    
-
+    } else {
+        points.forEach(element =>{
+            if(element.waitingTimeCountMinutes != 0){
+            let insertPoint = [Number(element.pointMap.split(',')[0]),Number(element.pointMap.split(',')[1])];
+            const draggableMarker  = new YMapDefaultMarker({
+            coordinates: insertPoint,
+            properties: {
+            hint: `${getComment(element.pointDesc)}`
+            },
+            color: '#FF0000'           
+            });
+            map.addChild(draggableMarker ); 
+            }
+        });
+    }  
+    const hint = new YMapHint({hint: (object) => object?.properties?.hint});
+    map.addChild(hint); 
+    hint.addChild(new HintWindow({}));
+    
     function getComment(descriptions) {
         let list = descriptions.split(" |");
         let result = "";
         list.forEach(element => {
+            if(element != "")
             result =  `${result}${element}<br>`;
         });
         return result;
@@ -148,6 +220,13 @@ async function initUpdateMap() {
     map.addChild(bottomControls);
     let typePlaces =await GetPlaceTypes();
     const buttoms = [];
+    const buttonBase = new YMapControlButton({
+        text: 'Travel',
+        color: '#fff',
+        background: '#007afce6',
+        onClick: () => window.location.href = `updateTravel.html?typeId=0&Scale=8&id=${travelId}`
+      });
+      buttoms.push(buttonBase);
     typePlaces.forEach(element => {
         const button = new YMapControlButton({
             text: element.name,
@@ -159,6 +238,25 @@ async function initUpdateMap() {
     });
     buttoms.forEach(element => 
         bottomControls.addChild(element));    
+
+    //Добавление точек в таблицу.
+    let Traveltable = document.getElementById('TravelsPoint');
+    points.forEach(element =>{
+        if(element.waitingTimeCountMinutes != 0){               
+        let TravelRow  = document.createElement('tr');
+        let Time = document.createElement('td');
+        Time.innerText = element.waitingTimeCountMinutes;
+        let Decs = document.createElement('td');
+        Decs.innerText = element.pointDesc;
+        let Coord = document.createElement('td');
+        Coord.innerText = element.pointMap;
+        TravelRow.appendChild(Time);
+        TravelRow.appendChild(Decs);
+        TravelRow.appendChild(Coord);
+        Traveltable.appendChild(TravelRow);
+        }
+    });
+
 
     function CreateLine(points)
      {
@@ -241,10 +339,9 @@ async function GetPoints() {
     if(result.ok)
         return await result.json();
 }
-async function deleteLastPoint() {
-    let travelId = new URLSearchParams(window.location.search).get('id');
-    let points = await GetPoints();
-    let point = points.pop();
+async function deleteLastPoint(event) {
+    let travelId = new URLSearchParams(window.location.search).get('id');    
+    let point = await GetPopPoints(event);
     let result = await fetch(`http://localhost:5200/api/point/${point.id}`,{
         method: 'DELETE',
         headers: {'TM-API-Key': 'F504ED6B-68AA-456C-B839-C1559ACED2EF'}
@@ -252,6 +349,28 @@ async function deleteLastPoint() {
     if(result.ok)
          window.location.href = `updateTravel.html?typeId=0&Scale=8&id=${travelId}`;
 }
+async function GetPopPoints(event)
+{
+    let result = "";
+    let points = await GetPoints();
+    let pointsEvent = [];
+    let pointsTrece = [];
+    points.forEach(element =>{
+        if (element.waitingTimeCountMinutes == 0){
+            pointsTrece.push(element);
+        } else {
+            pointsEvent.push(element);
+        }
+    });
+    
+    if( event == 1){
+        result = pointsEvent.pop();
+    } else {
+        result = pointsTrece.pop();
+    }
+    return result;
+}
+
 
 function goHome() {
     window.location.href = `index.html`;
