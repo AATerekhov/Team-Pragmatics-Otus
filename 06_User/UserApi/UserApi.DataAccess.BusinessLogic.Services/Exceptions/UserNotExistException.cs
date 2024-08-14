83aa43d3ac0c0c1591b4bd7e569bb4b0b1309881
AATerekhov <A.A.Terekhov@mail.ru>
2024-08-14 19:47:44 +0300

    Add Service Level Добавлен сервисный слой.

---
+++ 06_User/UserApi/UserApi.DataAccess.BusinessLogic.Services/Exceptions/UserNotExistException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserApi.DataAccess.BusinessLogic.Models.Exceptions
+{
+    public class UserNotExistException(Guid userId) : Exception($"Student with id: {userId} not exist.");
+  
+}
---
+++ 06_User/UserApi/UserApi.DataAccess.BusinessLogic.Services/Mapping/UserMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserApi.DataAccess.Entities;
+
+namespace UserApi.DataAccess.BusinessLogic.Models.Mapping
+{
+    public class UserMapping:  Profile
+    {
+        public UserMapping()
+        {
+            CreateMap<User, UserModel>();
+            CreateMap<CreateUserModel, User>();
+            CreateMap<UserModel, User>();
+        }
+    }
+}
