72fef9f3a7db595575ca1b90990d75dc8c244b91
AATerekhov <A.A.Terekhov@mail.ru>
2024-08-26 19:41:30 +0300

    Add user produser service CreateUser

---
+++ 06_User/UserApi/UserApi.DataAccess.BusinessLogic.Services/Dto/MessageCreateUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonNamespace
+{
+    public class MessageCreateUserDto
+    {
+        public string? Content { get; set; }
+    }
+}
---
+++ 06_User/UserApi/UserApi/Registrar.cs
@@ -0,0 +1,46 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using UserApi.Settings;
+using UserApi.DataAccess.EntityFramework;
+
+namespace UserApi
+{
+    public static class Registrar
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var aplicationSettings = configuration.Get<ApplicationSettings>();
+            services.AddSingleton(aplicationSettings);
+            return services.AddSingleton((IConfigurationRoot)configuration)
+                .ConfigureContext(aplicationSettings.ConnectionString);
+        }
+        public static IServiceCollection ConfigureContext(this IServiceCollection services,
+         string connectionString)
+        {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseNpgsql(connectionString,
+                    optionBuilder => optionBuilder.MigrationsAssembly("UserApi.DataAccess.EntityFramework"));
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            return services;
+        }
+        /// <summary>
+        /// Конфигурирование RMQ.
+        /// </summary>
+        /// <param name="configurator"> Конфигуратор RMQ. </param>
+        /// <param name="configuration"> Конфигурация приложения. </param>
+        public static void ConfigureRmq(IRabbitMqBusFactoryConfigurator configurator, IConfiguration configuration)
+        {
+            var rmqSettings = configuration.Get<ApplicationSettings>().RmqSettings;
+            configurator.Host(rmqSettings.Host,
+                rmqSettings.VHost,
+                h =>
+                {
+                    h.Username(rmqSettings.Login);
+                    h.Password(rmqSettings.Password);
+                });
+        }
+    }
+}
