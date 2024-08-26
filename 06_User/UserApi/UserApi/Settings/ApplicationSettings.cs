417888b6689d0897f38408543afb3c51ff055c94
AATerekhov <A.A.Terekhov@mail.ru>
2024-08-26 19:41:30 +0300

    Add user produser service CreateUser

---
+++ 06_User/UserApi/UserApi/Settings/ApplicationSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace UserApi.Settings
+{
+    public class ApplicationSettings
+    {
+        public string? ConnectionString { get; set; }
+        public RmqSettings? RmqSettings { get; set; }
+    }
+}
