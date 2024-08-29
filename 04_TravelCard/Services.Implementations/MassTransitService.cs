70023a600fb44c33a438cc98025f077312a026e3
KennyDew <dubrovskiynv@gmail.com>
2024-08-29 23:24:31 +0300

    Add MassTransitService

---
+++ 04_TravelCard/Services.Implementations/MassTransitService.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementations
+{
+    public class MasstransitService(IBusControl busControl) : IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await busControl.StartAsync(cancellationToken);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await busControl.StopAsync(cancellationToken);
+        }
+    }
+}
