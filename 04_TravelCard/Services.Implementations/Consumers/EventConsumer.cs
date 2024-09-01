8ae6a16f8e676751c38d3117c146b3d6f600a63d
KennyDew <dubrovskiynv@gmail.com>
2024-09-01 23:09:46 +0300

    Creating user by Consumer. Added check UserIsNotCreated

--- 04_TravelCard/Services.Implementations/Consumers/EventConsumer.cs
+++ 04_TravelCard/Services.Implementations/Consumers/EventConsumer.cs
@@ -10,6 +10,7 @@ using System.Net.Http.Json;
 using System;
 using System.Security.AccessControl;
 using Services.Contracts.User;
+using System.Net;
 
 namespace Services.Implementations.Consumers
 {
@@ -22,9 +23,10 @@ namespace Services.Implementations.Consumers
             //throw new ArgumentException("some error");
             await Task.Delay(TimeSpan.FromSeconds(1));
             Console.WriteLine("Consume value: {0}", context.Message.Content);
-            if (context.Message.Content != null)
+            var newUser = GetUserFromJSON(context.Message.Content);
+            var checkUserIsNotCreated = await CheckUserIsNotCreated(newUser.Id.ToString());
+            if (context.Message.Content != null && checkUserIsNotCreated)
             {
-                var newUser = GetUserFromJSON(context.Message.Content);
                 newUser.Deleted = false;
                 var response = await CreateUserFromBus(newUser);
             }
@@ -48,6 +50,12 @@ namespace Services.Implementations.Consumers
             JsonContent contentUser = JsonContent.Create(user);
             var response = await _travelClient.PostAsync("/User", contentUser);
             return await response.Content.ReadFromJsonAsync<UserDto>();
+        }
+
+        public async Task<bool> CheckUserIsNotCreated(string guid)
+        {
+            var response = await _travelClient.GetAsync($"/User/{guid}");
+            return response.StatusCode == HttpStatusCode.NoContent;
         }
     }
 }
